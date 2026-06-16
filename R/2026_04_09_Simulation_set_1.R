7b5b2e747e7f15982032642ace8d3c494236aef8
Daniel Schönberger <117173779+Daniel-1232@users.noreply.github.com>
2026-06-16 13:32:24 -0400

    Update 2026_04_09_Simulation_set_1.R

--- R/2026_04_09_Simulation_set_1.R
+++ R/2026_04_09_Simulation_set_1.R
@@ -3,7 +3,7 @@
 ## Set environment
 rm(list = ls()) #clear environment
 setwd("./")
-#setwd("C:/Users/danie/Desktop/PhD research/SOM package")
+#setwd("C:/Users/danie/Desktop/PhD research/Manuscripts/SOM package")
 source("https://raw.githubusercontent.com/rpyron/delim-SOM/refs/heads/dev2.0/R/2026_04_07_delim-SOM_2.0_functions.R")
 
 
